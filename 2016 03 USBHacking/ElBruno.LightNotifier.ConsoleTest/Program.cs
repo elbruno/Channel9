27cb50cfd7b4c631220351b799dc16f473de8c30
El Bruno <bcapuano@gmail.com>
2016-03-27 20:29:45 -0400

    + add base files for USB Hacking project demo

---
+++ 2016 03 USBHacking/ElBruno.LightNotifier.ConsoleTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ElBruno.LightNotifier.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lightController = new LightController();
+            
+            Console.WriteLine("Press Enter to turn ON the light");
+            lightController.TurnLight(true);
+            Console.ReadLine();
+
+            Console.WriteLine("Press Enter to turn OFF the light");
+            lightController.TurnLight(false);
+            Console.ReadLine();
+
+            Console.WriteLine("Press Enter to turn ON the light");
+            lightController.TurnLight(true);
+            Console.ReadLine();
+
+            Console.WriteLine("Press Enter to turn OFF the light");
+            lightController.TurnLight(false);
+            Console.ReadLine();
+
+        }
+    }
+}
