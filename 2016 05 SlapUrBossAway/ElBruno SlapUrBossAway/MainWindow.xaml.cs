6a847a35e0ac7200fca37f4ebd18b088cd941ac9
elbruno <bcapuano@gmail.com>
2016-05-23 22:59:47 -0400

    Complete sample for Channel9 demo

--- 2016 05 SlapUrBossAway/ElBruno SlapUrBossAway/MainWindow.xaml.cs
+++ 2016 05 SlapUrBossAway/ElBruno SlapUrBossAway/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ namespace ElBruno.SlapUrBossAway
     {
         private readonly Timer _timer = new Timer(1000);
         private string _bossEmail;
+        private LyncClient _client;
+        private ElBrunoListener _listener;
+        private Controller _controller;
+        private bool _swipeDetected;
 
         public MainWindow()
         {
@@ -27,7 +31,8 @@ namespace ElBruno.SlapUrBossAway
         {
             InitLyncClient();
             InitLeapMotion();
-            BossEmail = "brunocapuano@msn.com";
+            InitSlapMedia();
+            BossEmail = "your boss email here @ email.com";
             _timer.Elapsed += TimerElapsed;
             _timer.Enabled = true;
         }
@@ -38,20 +43,65 @@ namespace ElBruno.SlapUrBossAway
             _timer.Stop();
         }
 
+        private void InitSlapMedia()
+        {
+            var slapGif = AppDomain.CurrentDomain.BaseDirectory + @"\BarneySlapBet.gif";
+            MediaAnimagedGif.Source = new Uri(slapGif, UriKind.RelativeOrAbsolute);
+            MediaAnimagedGif.MediaEnded += MediaAnimagedGif_MediaEnded;
+        }
+        private void MediaAnimagedGif_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MediaAnimagedGif.Visibility = Visibility.Collapsed;
+        }
+
         void TimerElapsed(object sender, ElapsedEventArgs e)
         {
             Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(CheckLyncState));
         }
         void CheckLyncState()
         {
+            foreach (var conversation in _client.ConversationManager.Conversations)
+            {
+                foreach (var participant in conversation.Participants)
+                {
+                    if (!participant.Contact.Uri.ToLower().Contains(BossEmail.ToLower()) || !_swipeDetected) continue;
+                    conversation.End();
+                    _swipeDetected = false;
+                    MediaAnimagedGif.Visibility = Visibility.Visible;
+                    MediaAnimagedGif.Position = TimeSpan.Zero;
+                    MediaAnimagedGif.Play();
+                    Debug.WriteLine("Boss slapped !!!");
+                    return;
+                }
+            }
+
         }
 
         private void InitLeapMotion()
         {
+            _listener = new ElBrunoListener();
+            _controller = new Controller();
+            _controller.AddListener(_listener);
+            _listener.OnSwipeDetected += _listener_OnSwipeDetected;
+
+        }
+
+        private void _listener_OnSwipeDetected(bool swipeDetected)
+        {
+            _swipeDetected = swipeDetected;
+            Debug.WriteLine($"Swipe Detected: {_swipeDetected}");
         }
 
         private void InitLyncClient()
         {
+            try
+            {
+                _client = LyncClient.GetClient();
+            }
+            catch
+            {
+                MessageBox.Show("Microsoft Skype for Bussines does not appear to be running. Please start S4B.");
+            }
         }
 
         #region Notify PropertyChanged and Properties
