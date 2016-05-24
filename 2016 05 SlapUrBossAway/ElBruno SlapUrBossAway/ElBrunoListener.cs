93f343facd9076bb426955ca9d19502f1d539fd7
elbruno <bcapuano@gmail.com>
2016-05-23 22:25:36 -0400

    + base source code for SlapUrBossAway

---
+++ 2016 05 SlapUrBossAway/ElBruno SlapUrBossAway/ElBrunoListener.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Leap;
+
+namespace ElBruno.SlapUrBossAway
+{
+    public class ElBrunoListener : Listener
+    {
+        private readonly Object _thisLock = new Object();
+        public event Action<bool> OnSwipeDetected;
+
+
+        private void SafeWriteLine(String line)
+        {
+            lock (_thisLock)
+            {
+                Debug.WriteLine(line);
+            }
+        }
+
+        public override void OnInit(Controller controller)
+        {
+            SafeWriteLine("Initialized");
+        }
+
+        public override void OnConnect(Controller controller)
+        {
+            SafeWriteLine("Connected");
+            controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
+        }
+
+        public override void OnDisconnect(Controller controller)
+        {
+            SafeWriteLine("Disconnected");
+        }
+
+        public override void OnExit(Controller controller)
+        {
+            SafeWriteLine("Exited");
+        }
+
+        public override void OnFrame(Controller controller)
+        {
+            var frame = controller.Frame();
+            var gestures = frame.Gestures();
+            foreach (var swipe in from gesture in gestures where gesture.Type == Gesture.GestureType.TYPESWIPE select new SwipeGesture(gesture))
+            {
+                var gestureName = GetGestureNameFromSwipe(swipe);
+                SafeWriteLine(string.Format("Swipe id: {0}, name: {1}", swipe.Id, gestureName));
+                break;
+            }
+            if (frame.Gestures().Count > 0)
+                Task.Factory.StartNew(() => OnSwipeDetected(true));
+        }
+
+        private string GetGestureNameFromSwipe(SwipeGesture swipe)
+        {
+            var gestureName = "undefined";
+            var direction = swipe.Direction;
+            if (direction.Yaw > 0)
+            {
+                gestureName = "left to right";
+            }
+            if (direction.Yaw < 0)
+            {
+                gestureName = "right to left";
+            }
+            return gestureName;
+        }
+    }
+}
---
+++ 2016 05 SlapUrBossAway/ElBruno SlapUrBossAway/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
+using ElBruno.SlapUrBossAway.Annotations;
+using Leap;
+using Microsoft.Lync.Model;
+
+namespace ElBruno.SlapUrBossAway
+{
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        private readonly Timer _timer = new Timer(1000);
+        private string _bossEmail;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindowLoaded;
+            Unloaded += MainWindow_Unloaded;
+            DataContext = this;
+        }
+
+        void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            InitLyncClient();
+            InitLeapMotion();
+            BossEmail = "brunocapuano@msn.com";
+            _timer.Elapsed += TimerElapsed;
+            _timer.Enabled = true;
+        }
+
+        private void MainWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Elapsed -= TimerElapsed;
+            _timer.Stop();
+        }
+
+        void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(CheckLyncState));
+        }
+        void CheckLyncState()
+        {
+        }
+
+        private void InitLeapMotion()
+        {
+        }
+
+        private void InitLyncClient()
+        {
+        }
+
+        #region Notify PropertyChanged and Properties
+
+        public string BossEmail
+        {
+            get { return _bossEmail; }
+            set
+            {
+                if (value == _bossEmail) return;
+                _bossEmail = value;
+                OnPropertyChanged(nameof(BossEmail));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion  
+    }
+}
